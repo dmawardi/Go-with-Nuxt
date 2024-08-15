80908b6b574bf8e3b47216b1d06470c215a894fd
Dannar Mawardi <dannar@Dannars-MacBook-Pro.local>
2024-08-15 13:22:07 +0800

    Fixed issue with serving Vue app static files

--- backend/internal/routes/front.go
+++ backend/internal/routes/front.go
@@ -15,7 +15,7 @@ func ServeFrontEnd(router *chi.Mux, protected bool) *chi.Mux {
 	// Build fileserver using static directory
 	fileServer := http.FileServer(http.Dir("../frontend/dist"))
 	// Handle all calls to /static/* by stripping prefix and sending to file server
-	r.Handle("/front/*", http.StripPrefix("/front/", fileServer))
+	r.Handle("/*", http.StripPrefix("/", fileServer))
 
 	r.Group(func(mux chi.Router) {
 		// Set to use JWT authentication if protected
